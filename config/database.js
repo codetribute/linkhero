d742056c02c923abe6c1ffd36198c89df103aeaf
codetribute <codetribute@gmail.com>
2014-07-22 15:44:37 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ exports.ConStr = function(app){
     }
     var generate_mongo_url = function(obj){
         obj.hostname = (obj.hostname || 'localhost');
-        obj.port = (obj.port || 27017);
+        obj.port = (obj.port || 27017); 
         obj.db = (obj.db || 'test');
         if(obj.username && obj.password){
             return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
